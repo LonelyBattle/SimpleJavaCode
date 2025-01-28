0c6f53e5b977e4381cc36fa415c299fd500e2ad6
Lenovo <3114665232@qq.com>
2025-01-29 00:24:32 +0800

    添加websocket使用示例代码

---
+++ websocket/src/main/java/com/zs/Application.java
@@ -0,0 +1,16 @@
+package com.zs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: LonelyBattle
+ * @create: 2025-01-29 00:09
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
